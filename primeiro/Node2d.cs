6c3df36107fe3fa517e2d6d4edffd9991fcee799
Ramon Couto <ramon.couto08.s@gmail.com>
2025-04-26 23:41:01 -0300

    atualizando o game

---
+++ primeiro/Node2d.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Node2d : Node2D
+{
+	string teste = "";
+
+	int inteiro = 0;
+
+	public override void _Ready(){
+		teste = "Deu certo";
+		GD.Print(teste);
+	} 
+	public override void _Process(double delta){
+
+	}
+}
