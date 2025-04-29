a5006451d5493457be8c1f92a9d0c299e9f35faa
Ramon Couto <ramon.couto08.s@gmail.com>
2025-04-29 02:01:08 -0300

    testando

--- Estefânia do Norte/Node2d.cs
+++ Estefânia do Norte/Node2d.cs
@@ -8,10 +8,10 @@ public partial class Node2d : Node2D
 	int inteiro = 0;
 
 	public override void _Ready(){
-		teste = "Deu certo";
+		teste = "Deu certo teste test tes";
 		GD.Print(teste);
 	} 
 	public override void _Process(double delta){
 
-	}
+	} 
 }
